8cd72338b6d962fb65ca63fdd5522b2916b98ed2
SofiaG <90490408+00026521SofiaG@users.noreply.github.com>
2023-06-27 17:23:10 -0600

    :card_file_box:Docker file added so the Mongo data base runs on a docker container

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ const mongoose = require("./config/mongoose");
 //router
 const apiRouter = require("./routes/api/general.router");
 
+//CORS
+const cors = require("cors");
 
 var app = express();
 mongoose.connect();
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
